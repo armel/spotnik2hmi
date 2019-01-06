72b51c94a2c25a090abc0a480ae89ef4acb38ba6
F8ASB <f8asb@hotmail.fr>
2019-01-06 19:59:59 +0100

    Update maj.sh

--- maj.sh
+++ maj.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/fonctions.py
+wget  https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/spotnik2hmi.py
 wget  https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/install.sh
 
