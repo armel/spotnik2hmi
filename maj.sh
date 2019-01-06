137f3a3a2f15655c4bd0a60a279c131a82ef0df3
F8ASB <f8asb@hotmail.fr>
2019-01-06 20:58:04 +0100

    update

--- maj.sh
+++ maj.sh
@@ -1,6 +1,6 @@
-mv fonctions.py /old_version/
-mv spotnik2hmi.py /old_version/
-mv install.sh /old_version/
+mv fonctions.py /opt/spotnik/spotnik2hmi/old_version/
+mv spotnik2hmi.py /opt/spotnik/spotnik2hmi/old_version/
+mv install.sh /opt/spotnik/spotnik2hmi/old_version/
 
 wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/fonctions.py
 wget  https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/spotnik2hmi.py
