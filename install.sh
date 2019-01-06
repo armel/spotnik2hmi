498f834a2476ca1d58119121d62082f31ad2dc98
F8ASB <f8asb@hotmail.fr>
2019-01-06 19:09:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,14 +38,14 @@ pip install psutil
 echo "INSTALLATION COMPLETE !"
 
 echo "INSTALLATION scripts python"
-git clone https://github.com/F8ASB/spotnik2hmi.git /opt/spotnik/spotnik2hmi/
+git clone https://github.com/F8ASB/spotnik2hmi.git /opt/spotnik/
 
 chmod +x /opt/spotnik/spotnik2hmi/spotnik2hmi.py
 
 echo "INSTALLATION COMPLETE !"
 
 echo "INSTALLATION UTILITAIRE METAR"
-git clone https://github.com/python-metar/python-metar.git /opt/spotnik/spotnik2hmi/python-metar/
+git clone https://github.com/python-metar/python-metar.git /opt/spotnik/spotnik2hmi/
 echo "INSTALLATION COMPLETE !"
 
 PORT=$(whiptail --title "Choix du Port de communication" --radiolist \
@@ -57,7 +57,7 @@ PORT=$(whiptail --title "Choix du Port de communication" --radiolist \
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
 
-sed -i '/make start/a \python /opt/spotnik/spotnik2hmi/spotnik2hmi.py '$PORT' 9600' /etc/rc.local
+echo 'python /opt/spotnik/spotnik2hmi.spotnik2hmi.py' $PORT '9600' >> /etc/rc.local
 else
     echo "Vous avez annulé"
 fi
