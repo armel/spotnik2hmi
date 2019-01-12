c5940b6612d57f9ea1f4e316e5f4d3fba102bdcc
F8ASB <f8asb@hotmail.fr>
2019-01-12 07:23:18 +0100

    Add files via upload

---
+++ maj.sh
@@ -0,0 +1,10 @@
+#stamp=date(%Y%m%d)
+mv fonctions.py /opt/spotnik/spotnik2hmi/old_version/fonctions`date +%d%m%Y`.py
+mv spotnik2hmi.py /opt/spotnik/spotnik2hmi/old_version/spotnik2hmi`date +%d%m%Y`.py
+mv install.sh /opt/spotnik/spotnik2hmi/old_version/install`date +%d%m%Y`.sh
+
+wget https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/fonctions.py
+wget  https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/spotnik2hmi.py
+wget  https://raw.githubusercontent.com/F8ASB/spotnik2hmi/master/install.sh
+
+
