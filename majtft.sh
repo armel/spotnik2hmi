3e8131a12a813d140f52d0feb5673bbd199f8e72
F8ASB <f8asb@hotmail.fr>
2019-01-13 15:56:08 +0100

    Add files via upload

---
+++ majtft.sh
@@ -0,0 +1,29 @@
+echo "Sauvergarde des anciens fichiers tft dans old_version"
+mv /nextion/NX3224T024.tft /opt/spotnik/spotnik2hmi/old_version/NX3224T024`date +%d%m%Y`.tft
+mv /nextion/NX3224K024.tft /opt/spotnik/spotnik2hmi/old_version/NX3224K024`date +%d%m%Y`.tft
+mv /nextion/NX3224T028.tft /opt/spotnik/spotnik2hmi/old_version/NX3224T028`date +%d%m%Y`.tft
+mv /nextion/NX3224K028.tft /opt/spotnik/spotnik2hmi/old_version/NX3224K028`date +%d%m%Y`.tft
+mv /nextion/NX4024T032.tft /opt/spotnik/spotnik2hmi/old_version/NX4024T032`date +%d%m%Y`.tft
+mv /nextion/NX4024K032.tft /opt/spotnik/spotnik2hmi/old_version/NX4024K032`date +%d%m%Y`.tft
+mv /nextion/NX4832T035.tft /opt/spotnik/spotnik2hmi/old_version/NX4832K035`date +%d%m%Y`.tft
+mv /nextion/NX4832K035.tft /opt/spotnik/spotnik2hmi/old_version/NX4832K035`date +%d%m%Y`.tft
+mv /nextion/NX8048T050.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K050`date +%d%m%Y`.tft
+mv /nextion/NX8048K050.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K050`date +%d%m%Y`.tft
+mv /nextion/NX8048T070.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K070`date +%d%m%Y`.tft
+mv /nextion/NX8048K070.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K070`date +%d%m%Y`.tft
+
+
+echo "telechargement des derniers fichiers tft"
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX3224T024.tft /nextion/NX3224T024.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX3224K024.tft /nextion/NX3224K024.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX3224T028.tft /nextion/NX3224T028.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX3224K028.tft /nextion/NX3224K028.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX4024T032.tft /nextion/NX4024T032.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX4024K032.tft /nextion/NX4024K032.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX4832T035.tft /nextion/NX4832T035.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX4832K035.tft /nextion/NX4832K035.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX8048T050.tft /nextion/NX8048T050.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX8048K050.tft /nextion/NX8048K050.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX8048T070.tft /nextion/NX8048T070.tft
+wget https://github.com/F8ASB/spotnik2hmi/blob/master/nextion/NX8048K070.tft /nextion/NX8048K070.tft
+
