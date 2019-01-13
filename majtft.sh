69ac45e76f64218a003bd9457c46c8151df6b170
F8ASB <f8asb@hotmail.fr>
2019-01-13 16:06:01 +0100

    Update majtft.sh

--- majtft.sh
+++ majtft.sh
@@ -1,16 +1,16 @@
 echo "Sauvergarde des anciens fichiers tft dans old_version"
-mv /nextion/NX3224T024.tft /opt/spotnik/spotnik2hmi/old_version/NX3224T024`date +%d%m%Y`.tft
-mv /nextion/NX3224K024.tft /opt/spotnik/spotnik2hmi/old_version/NX3224K024`date +%d%m%Y`.tft
-mv /nextion/NX3224T028.tft /opt/spotnik/spotnik2hmi/old_version/NX3224T028`date +%d%m%Y`.tft
-mv /nextion/NX3224K028.tft /opt/spotnik/spotnik2hmi/old_version/NX3224K028`date +%d%m%Y`.tft
-mv /nextion/NX4024T032.tft /opt/spotnik/spotnik2hmi/old_version/NX4024T032`date +%d%m%Y`.tft
-mv /nextion/NX4024K032.tft /opt/spotnik/spotnik2hmi/old_version/NX4024K032`date +%d%m%Y`.tft
-mv /nextion/NX4832T035.tft /opt/spotnik/spotnik2hmi/old_version/NX4832K035`date +%d%m%Y`.tft
-mv /nextion/NX4832K035.tft /opt/spotnik/spotnik2hmi/old_version/NX4832K035`date +%d%m%Y`.tft
-mv /nextion/NX8048T050.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K050`date +%d%m%Y`.tft
-mv /nextion/NX8048K050.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K050`date +%d%m%Y`.tft
-mv /nextion/NX8048T070.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K070`date +%d%m%Y`.tft
-mv /nextion/NX8048K070.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K070`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX3224T024.tft /opt/spotnik/spotnik2hmi/old_version/NX3224T024`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX3224K024.tft /opt/spotnik/spotnik2hmi/old_version/NX3224K024`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX3224T028.tft /opt/spotnik/spotnik2hmi/old_version/NX3224T028`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX3224K028.tft /opt/spotnik/spotnik2hmi/old_version/NX3224K028`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX4024T032.tft /opt/spotnik/spotnik2hmi/old_version/NX4024T032`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX4024K032.tft /opt/spotnik/spotnik2hmi/old_version/NX4024K032`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX4832T035.tft /opt/spotnik/spotnik2hmi/old_version/NX4832K035`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX4832K035.tft /opt/spotnik/spotnik2hmi/old_version/NX4832K035`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX8048T050.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K050`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX8048K050.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K050`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX8048T070.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K070`date +%d%m%Y`.tft
+mv /opt/spotnik/spotnik2hmi/nextion/NX8048K070.tft /opt/spotnik/spotnik2hmi/old_version/NX8048K070`date +%d%m%Y`.tft
 
 
 echo "telechargement des derniers fichiers tft"
