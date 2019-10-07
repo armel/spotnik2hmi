b25272af4e52a88a947d82756c6bad3179deb78e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-08 01:50:38 +0200

    Refactoring code

--- fonctions.py
+++ fonctions.py
@@ -199,7 +199,7 @@ def page(nompage):
     eof = '\xff\xff\xff'
     appelpage = 'page ' + nompage + eof
     port.write(appelpage)
-    print appelpage.strip()
+    print appelpage.replace(eof, '')
 
 #Fonction recherche de nom de ville selon code ICAO
 def get_city():
