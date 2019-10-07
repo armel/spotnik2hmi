f5170336edf330abe9dd7b158e34a0fef3175bda
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-10-07 14:32:31 +0200

    Refactoring code

--- spotnik2hmi.py
+++ spotnik2hmi.py
@@ -478,7 +478,7 @@ while True:
 #QSYSALON
 
     s = ''.join(e for e in s if e.isalnum())
-    print '>>>>>>>' + s
+    print '>>>>>>>' + s, s[-3:]
     if s[-3:] not in room_list:
         ecrire('page200.t3.txt','Mode autonome')
     else:
